38def25b834c9838fd188adba2e28bfcf1e07241
jdykst02 <jdykstra04@gmail.com>
2023-02-06 12:52:23 -0500

    Finished code for lab test 1

--- src/labtest1/LabTest1.java
+++ src/labtest1/LabTest1.java
@@ -1,49 +1,70 @@
 package labtest1;
-
+/**
+ * 
+ * @author Jacob 
+ * @version 1.0.1
+ * @since 2023-2-6
+ */
 
 public class LabTest1 {
+    /** 
+    * @param unsorted[]
+    */ 
     void sortFunction1(int unsorted[])
     {
         int len = unsorted.length;
-        for (int i = 0; i <= len; i++) 
+        for (int i = 0; i <= len-1; i++) 
         { 
             int index = i;
-            for (int j = i+1; j < len-1; j++) 
-                if (unsorted[j] < unsorted[index])
+            for (int j = i+1; j < len; j++){
+                if (unsorted[j] < unsorted[index]){
                     index = j;
+                }
+            }           
             int t = unsorted[index];
             unsorted[index] = unsorted[i];
             unsorted[i] = t;
+            
         }
     }
- 
+    /** 
+    * @param unsorted[]
+    */ 
     void sortFunction2(int unsorted[])
     {
-        int length = unsorted.length;
-        for (int i = 0; i < length - 1; i++)
-            for (int j = 0; j < length - i - 1; j++)
+        int length = unsorted.length;//
+        for (int i = 0; i < length; i++)//
+            for (int j = 0; j < length - 1; j++)//
                 if (unsorted[j] > unsorted[i]) { 
                     int t = unsorted[j];
-                    unsorted[j] = unsorted[j + 1];
-                    unsorted[j + 1] = t;
+                    unsorted[j] = unsorted[i];
+                    unsorted[i] = t;
                 }
     }
-     
+    /** 
+    * @param unsorted[]
+    */ 
     void sortFunction3(int unsorted[])
     {
         int ln = unsorted.length;
-        for (int i = 1; i < ln; ++i) {
-            int item = unsorted[i];
+        for (int i = 0; i < ln-1; i++) {
+            
             int j = i + 1; 
             
-            while (j > 0 && unsorted[j] > item) { 
-                unsorted[j + 1] = unsorted[j];
+            while (j > 0 && unsorted[j] < unsorted[j-1]) {
+                int item = unsorted[j-1];
+                unsorted[j - 1] = unsorted[j];
+                unsorted[j] = item;
                 j = j - 1;
+                
             }
-            unsorted[j + 1] = item;
+            
+           
         }
     }
-     
+    /** 
+    * @param arr[]
+    */ 
     void printArray(int arr[])
     {
         int n = arr.length;
