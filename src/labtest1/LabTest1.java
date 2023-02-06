aab694591e08165fcecf12b69e0609886884f612
aariawann <aariawan@uoguelph.ca>
2023-02-06 13:14:48 -0500

    LabTest1 Aris Ariawan

--- src/labtest1/LabTest1.java
+++ src/labtest1/LabTest1.java
@@ -2,57 +2,11 @@ package labtest1;
 
 
 public class LabTest1 {
-    void sortFunction1(int unsorted[])
-    {
-        int len = unsorted.length;
-        for (int i = 0; i <= len; i++) 
-        { 
-            int index = i;
-            for (int j = i+1; j < len-1; j++) 
-                if (unsorted[j] < unsorted[index])
-                    index = j;
-            int t = unsorted[index];
-            unsorted[index] = unsorted[i];
-            unsorted[i] = t;
-        }
-    }
- 
-    void sortFunction2(int unsorted[])
-    {
-        int length = unsorted.length;
-        for (int i = 0; i < length - 1; i++)
-            for (int j = 0; j < length - i - 1; j++)
-                if (unsorted[j] > unsorted[i]) { 
-                    int t = unsorted[j];
-                    unsorted[j] = unsorted[j + 1];
-                    unsorted[j + 1] = t;
-                }
-    }
-     
-    void sortFunction3(int unsorted[])
-    {
-        int ln = unsorted.length;
-        for (int i = 1; i < ln; ++i) {
-            int item = unsorted[i];
-            int j = i + 1; 
-            
-            while (j > 0 && unsorted[j] > item) { 
-                unsorted[j + 1] = unsorted[j];
-                j = j - 1;
-            }
-            unsorted[j + 1] = item;
-        }
-    }
-     
-    void printArray(int arr[])
-    {
-        int n = arr.length;
-        for (int i=0; i<n; ++i)
-            System.out.print(arr[i]+" ");
-        System.out.println();
-    }
- 
     
+    /**
+     * Arrays are sorted then printed
+     * @param args
+     */
     public static void main(String[] args) {
         LabTest1 obj = new LabTest1();
         
@@ -60,8 +14,6 @@ public class LabTest1 {
         obj.sortFunction1(unsorted);
         System.out.println("Sorted array1 : ");
         obj.printArray(unsorted);
-        
-        
         
         int unsorted2[] = {64,25,12,22,11};
         obj.sortFunction2(unsorted2);
@@ -75,4 +27,75 @@ public class LabTest1 {
        
     }
     
+    /**
+     * sorts an array of integers from least to greatest
+     * @param unsorted - array to be sorted
+     */
+    public void sortFunction1(int unsorted[])
+    {
+        int len = unsorted.length;
+        for (int i = 0; i <= len; i++) 
+        { 
+            int index = i;
+            for (int j = i+1; j < len; j++) 
+                if (unsorted[j] < unsorted[index]){
+                    
+                    int t = unsorted[j];
+                
+                    unsorted[j] = unsorted[i];
+                    unsorted[i] = t;
+                }
+        }
+    }
+ 
+    /**
+     * sorts an array of integers from greatest to least
+     * @param unsorted - array to be sorted
+     */
+    public void sortFunction2(int unsorted[])
+    {
+        int length = unsorted.length;
+        for (int i = 0; i < length; i++)
+            for (int j = i+1; j < length; j++)
+                if (unsorted[j] > unsorted[i]) { 
+                    int t = unsorted[i];
+                    unsorted[i] = unsorted[j];
+                    unsorted[j] = t;
+                }
+    }
+     
+    /**
+     * sorts an array of integers from greatest to least 
+     * @param unsorted - array to be sorted
+     */
+    public void sortFunction3(int unsorted[])
+    {
+        int ln = unsorted.length;
+        for (int i = 1; i < ln; ++i) {
+            
+            int item = unsorted[i];
+            
+            int j = i + 1; 
+            
+            while (j < ln && unsorted[j] > item) { 
+                
+                unsorted[i] = unsorted[j];
+                
+                unsorted[j] = item;
+            }
+        }
+    }
+     
+    /**
+     * prints an Array
+     * @param arr - array to be printed
+     */
+    public void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i]+" ");
+        System.out.println();
+    }
+ 
 }
