35303be2dfec33b7e5eea659d31c00ba9cc966d6
rhawki01 <rickh7@live.com>
2023-02-07 11:43:41 -0500

    Submitted @11:43am

--- src/labtest1/LabTest1.java
+++ src/labtest1/LabTest1.java
@@ -1,18 +1,23 @@
 package labtest1;
 import java.util.Arrays;
-
+/**
+*@author Richard Hawkins
+*@since 2023-02-07
+*/
 public class LabTest1 {
     void sortFunction4(int unsorted[])
     {
-        int index = 0;
+        int index = 0, temp = 0;
         int n = unsorted.length;
-        while (index <= n) { 
-            if (index == 0)
-                index++; 
-            if (unsorted[index - 1] > unsorted[index]) 
+        while (index < n) { 
+            if (index == 0){
                 index++;
+            }
+            
+            if (unsorted[index - 1] < unsorted[index]){ 
+                index++;
+            }
             else {
-                int temp = 0;
                 temp = unsorted[index];
                 unsorted[index] = unsorted[index - 1];
                 unsorted[index - 1] = temp;
@@ -25,30 +30,34 @@ public class LabTest1 {
     {
         int n = unsorted.length;
         int min = unsorted[0];
-        int max = unsorted[n];
+        int max = unsorted[n-1];
         int range, i, j, index;
  
-        for(int a=0; a<n; a++)
+        for(int a = 0; a < n; a++)
         {
-            if(unsorted[a] < max) 
+            if(unsorted[a] > max){ 
                 max = unsorted[a];
-            if(unsorted[a] > min) 
+            }
+            else if(unsorted[a] < min){
                 min = unsorted[a];
+            }
         }
  
         range = max - min + 1;
         int[] phole = new int[range];
         Arrays.fill(phole, 0);
  
-        for(i = 0; i<n; i++)
+        for(i = 0; i<n; i++){
             phole[unsorted[i] - min]++;
- 
+        }
          
         index = 0;
  
-        for(j = 0; j<range; j++)
-            while(phole[j]-->0)
+        for(j = 0; j<range; j++){
+            while(phole[j]-->0){
                 unsorted[index++]=j+min;
+            }
+        }
     }
      
     void sortFunction6(int unsorted[]) 
@@ -58,11 +67,11 @@ public class LabTest1 {
         while (!isSorted)
         {
             isSorted = true;
-            int temp =0;
+            int temp = 0;
  
-            for (int i=1; i<=n-2; i=i+2)
+            for (int i = 1; i <= n-2; i +=2)
             {
-                if (unsorted[i + 1] > unsorted[i]) 
+                if (unsorted[i + 1] < unsorted[i]) 
                 {
                     temp = unsorted[i];
                     unsorted[i] = unsorted[i+1];
@@ -71,7 +80,7 @@ public class LabTest1 {
                 }
             }
  
-            for (int i=0; i<=n-2; i=i+2)
+            for (int i=0; i<=n-2; i+=2)
             {
                 if (unsorted[i] > unsorted[i+1])
                 {
@@ -87,9 +96,11 @@ public class LabTest1 {
     void printArray(int arr[])
     {
         int n = arr.length;
-        for (int i=0; i<n; ++i)
+        for (int i=0; i<n; i++){
             System.out.print(arr[i]+" ");
-        System.out.println();
+            
+        }
+        System.out.print("\n");
     }
  
     
