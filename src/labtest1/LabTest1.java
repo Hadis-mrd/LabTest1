6ab2dab7d876e3f1e3ee6025619d347cf0ce70d0
tylerwarwick <tylerwarwickwork@gmail.com>
2023-02-07 11:51:10 -0500

    Fixed

--- src/labtest1/LabTest1.java
+++ src/labtest1/LabTest1.java
@@ -1,15 +1,31 @@
 package labtest1;
 import java.util.Arrays;
 
+
+/**
+ * <h2>Sort an Array Using 3 Different Methods</h2>
+ * The LabTest1 program implements an application that 
+ * sorts an array using 3 different sorting
+ * algorithms and prints the sorted arrays to the screen 
+ * 
+ * 
+ * @author tylerwarwick
+ * @version 2.0
+ */
 public class LabTest1 {
+    
+    /** 
+     * This method is used to sort an array
+     * @param unsorted array passed to sortFunction4
+     */
     void sortFunction4(int unsorted[])
     {
         int index = 0;
         int n = unsorted.length;
-        while (index <= n) { 
+        while (index < n) { 
             if (index == 0)
                 index++; 
-            if (unsorted[index - 1] > unsorted[index]) 
+            if (unsorted[index - 1] < unsorted[index]) 
                 index++;
             else {
                 int temp = 0;
@@ -20,19 +36,23 @@ public class LabTest1 {
             }
         }
     }
- 
+    
+    /** 
+     * This method is used to sort an array
+     * @param unsorted array passed to sortFunction5
+     */
     void sortFunction5(int unsorted[]) 
     {
         int n = unsorted.length;
         int min = unsorted[0];
-        int max = unsorted[n];
+        int max = unsorted[n-1];
         int range, i, j, index;
  
         for(int a=0; a<n; a++)
         {
-            if(unsorted[a] < max) 
+            if(unsorted[a] > max) 
                 max = unsorted[a];
-            if(unsorted[a] > min) 
+            if(unsorted[a] < min) 
                 min = unsorted[a];
         }
  
@@ -47,10 +67,14 @@ public class LabTest1 {
         index = 0;
  
         for(j = 0; j<range; j++)
-            while(phole[j]-->0)
+            while(phole[j]-- > 0)
                 unsorted[index++]=j+min;
     }
      
+    /** 
+     * This method is used to sort an array
+     * @param unsorted array passed to sortFunction6
+     */
     void sortFunction6(int unsorted[]) 
     {
         boolean isSorted = false;
@@ -62,7 +86,7 @@ public class LabTest1 {
  
             for (int i=1; i<=n-2; i=i+2)
             {
-                if (unsorted[i + 1] > unsorted[i]) 
+                if (unsorted[i + 1] < unsorted[i]) 
                 {
                     temp = unsorted[i];
                     unsorted[i] = unsorted[i+1];
@@ -84,6 +108,10 @@ public class LabTest1 {
         }
     }
      
+    /** 
+     * This method is used to print an array to the screen
+     * @param arr array passed to be printed by function
+     */
     void printArray(int arr[])
     {
         int n = arr.length;
@@ -92,7 +120,10 @@ public class LabTest1 {
         System.out.println();
     }
  
-    
+    /**
+     * This is the main method that makes use of the sorting and printing methods
+     * @param args unused
+     */
     public static void main(String[] args) {
         LabTest1 obj = new LabTest1();
         
