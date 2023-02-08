93fa5b05751da37b30ecfb3b35dd4d564cf6c2e8
nciublea <nciublea@uoguelph.ca>
2023-02-08 16:56:51 -0500

    Feb 8th 4:56 pm Revised code with required changes

--- src/labtest1/LabTest1.java
+++ src/labtest1/LabTest1.java
@@ -1,14 +1,23 @@
 package labtest1;
 
+/**
+ * 
+ * @author  Nicholas Ciublea
+ * @since   2023-02-08
+ */
 public class LabTest1 {
-	
+
+    /**
+     * 
+     * @param unsorted an array of random numbers to be sorted
+     */
     void sortFunction7(int unsorted[]) 
     {
         int len = unsorted.length;
-        for (int i = 0; i <= len; i++) 
+        for (int i = 0; i < len; i++) 
         { 
             int index = i;
-            for (int j = i+1; j < len-1; j++) 
+            for (int j = i+1; j < len; j++) 
                 if (unsorted[j] < unsorted[index])
                     index = j;
             int t = unsorted[index];
@@ -17,39 +26,46 @@ public class LabTest1 {
         }
     }
  
-    void sortFunction8(int unsorted[]) 
-    {
-        int ln = unsorted.length;
-        for (int i = 1; i < ln; ++i) {
-            int item = unsorted[i];
-            int j = i + 1; 
-            
-            while (j > 0 && unsorted[j] > item) { 
-                unsorted[j + 1] = unsorted[j];
-                j = j - 1;
-            }
-            unsorted[j + 1] = item;
+    /**
+     * 
+     * @param unsorted an array of random numbers to be sorted
+     */
+void sortFunction8(int unsorted[]) 
+{
+    int ln = unsorted.length;
+    for (int i = 1; i < ln; ++i) {
+        int item = unsorted[i];
+        int j = i - 1; 
+        while (j >= 0 && unsorted[j] > item) { 
+            unsorted[j + 1] = unsorted[j];
+            j = j - 1;
         }
+        unsorted[j + 1] = item;
     }
+}
     
-    void sortFunction9(int unsorted[])
-    {
-        int index = 0;
-        int n = unsorted.length;
-        while (index <= n) { 
-            if (index == 0)
-                index++; 
-            if (unsorted[index-1] > unsorted[index])
-                index++;
-            else {
-                int temp = unsorted[index];
-                unsorted[index] = unsorted[index - 1];
-                unsorted[index - 1] = temp;
-                index--;
-            }
+/**
+ * 
+ * @param unsorted an array of random integers to be sorted
+ */
+void sortFunction9(int unsorted[])
+{
+    int ln = unsorted.length;
+    for (int i = 1; i < ln; ++i) {
+        int item = unsorted[i];
+        int j = i - 1; 
+        while (j >= 0 && unsorted[j] > item) { 
+            unsorted[j + 1] = unsorted[j];
+            j = j - 1;
         }
+        unsorted[j + 1] = item;
     }
+}
     
+/**
+ * 
+ * @param arr an array of integers to be printed
+ */
     void printArray(int arr[])
     {
         int n = arr.length;
@@ -59,24 +75,29 @@ public class LabTest1 {
     }
  
     
-    public static void main(String[] args) {
-        LabTest1 obj = new LabTest1();
-        
-        int unsorted7[] = {74,25,13,22,8};
-        obj.sortFunction7(unsorted7);
-        System.out.println("Sorted array7 : ");
-        obj.printArray(unsorted7);
-        
-        int unsorted8[] = {74,25,13,22,8};
-        obj.sortFunction8(unsorted8);
-        System.out.println("Sorted array8 : ");
-        obj.printArray(unsorted8);
-        
-        int unsorted9[] = {74,25,13,22,8};
-        obj.sortFunction9(unsorted9);
-        System.out.println("Sorted array9 : ");
-        obj.printArray(unsorted9);
-       
-    }
-    
+public static void main(String[] args) {
+    LabTest1 obj = new LabTest1();
+
+    int unsorted7[] = {74, 25, 13, 22, 8};
+    System.out.println("Input Array 7: ");
+    obj.printArray(unsorted7);
+    obj.sortFunction7(unsorted7);
+    System.out.println("Sorted array7 : ");
+    obj.printArray(unsorted7);
+
+    int unsorted8[] = {74, 25, 13, 22, 8};
+    System.out.println("Input Array 8: ");
+    obj.printArray(unsorted8);
+    obj.sortFunction8(unsorted8);
+    System.out.println("Sorted array8 : ");
+    obj.printArray(unsorted8);
+
+    int unsorted9[] = {74, 25, 13, 22, 8};
+    System.out.println("Input Array 9: ");
+    obj.printArray(unsorted9);
+    obj.sortFunction9(unsorted9);
+    System.out.println("Sorted array9 : ");
+    obj.printArray(unsorted9);
+
+}
 }
