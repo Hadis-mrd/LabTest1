f0bed8f00f2ea034e67a9220cd17629376ae3692
vvictore <vanessavictorekpe@gmail.com>
2023-02-08 17:05:08 -0500

    Slight modification

--- src/labtest1/LabTest1.java
+++ src/labtest1/LabTest1.java
@@ -1,14 +1,19 @@
 package labtest1;
 
 public class LabTest1 {
-	
+    /**
+     * @author Vanessa Ebifie Victor-Ekpe
+     * @version 2.0
+     * @since 2023-02-08
+     * @param unsorted 
+     */
     void sortFunction7(int unsorted[]) 
     {
         int len = unsorted.length;
-        for (int i = 0; i <= len; i++) 
+        for (int i = 0; i < len; i++) //changed <= to <
         { 
             int index = i;
-            for (int j = i+1; j < len-1; j++) 
+            for (int j = i+1; j <= len-1; j++) //changed j<len-1 to j<=len-1
                 if (unsorted[j] < unsorted[index])
                     index = j;
             int t = unsorted[index];
@@ -16,30 +21,36 @@ public class LabTest1 {
             unsorted[i] = t;
         }
     }
- 
+ /**
+  * 
+  * @param unsorted 
+  */
     void sortFunction8(int unsorted[]) 
     {
         int ln = unsorted.length;
         for (int i = 1; i < ln; ++i) {
             int item = unsorted[i];
-            int j = i + 1; 
+            int j = i - 1; //changed i+1 to i-1
             
-            while (j > 0 && unsorted[j] > item) { 
+            while (j >= 0 && unsorted[j] > item) { //changed j>0 to j>=0
                 unsorted[j + 1] = unsorted[j];
                 j = j - 1;
             }
             unsorted[j + 1] = item;
         }
     }
-    
+    /**
+     * 
+     * @param unsorted 
+     */
     void sortFunction9(int unsorted[])
     {
         int index = 0;
         int n = unsorted.length;
-        while (index <= n) { 
+        while (index < n) { //changed <= to <
             if (index == 0)
                 index++; 
-            if (unsorted[index-1] > unsorted[index])
+            if (unsorted[index-1] <= unsorted[index]) //changed < to <=
                 index++;
             else {
                 int temp = unsorted[index];
@@ -49,7 +60,10 @@ public class LabTest1 {
             }
         }
     }
-    
+    /**
+     * @see "No changes were made here"
+     * @param arr 
+     */
     void printArray(int arr[])
     {
         int n = arr.length;
@@ -58,7 +72,10 @@ public class LabTest1 {
         System.out.println();
     }
  
-    
+    /**
+     * @see "this is main"
+     * @param args 
+     */
     public static void main(String[] args) {
         LabTest1 obj = new LabTest1();
         
