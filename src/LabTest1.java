6164e211abf0786f910f73677f8d3d4a4f372e8b
Haadi-Rehan <haadi4r@gmail.com>
2023-02-09 13:07:55 -0500

    Syed Haadi Rehan - Lab Test 1

--- src/LabTest1.java
+++ src/LabTest1.java
@@ -1,14 +1,21 @@
-package labtest1;
-
+/**
+ * This is a code for sorting unsorted arrays in a ordered sequence form
+ * @author HAADI REHAN
+ * @version 1.00
+ */
+/**
+ * 
+ * @author HAADI REHAN
+ */
 public class LabTest1 {
-	
+
     void sortFunction7(int unsorted[]) 
     {
         int len = unsorted.length;
-        for (int i = 0; i <= len; i++) 
+        for (int i = 0; i < len - 1; i++) 
         { 
             int index = i;
-            for (int j = i+1; j < len-1; j++) 
+            for (int j = i + 1; j < len; j++) 
                 if (unsorted[j] < unsorted[index])
                     index = j;
             int t = unsorted[index];
@@ -16,40 +23,45 @@ public class LabTest1 {
             unsorted[i] = t;
         }
     }
- 
+/**
+ * Here we used sort function 8 and made for loops to order the array
+ * @param unsorted 
+ */
     void sortFunction8(int unsorted[]) 
     {
         int ln = unsorted.length;
         for (int i = 1; i < ln; ++i) {
             int item = unsorted[i];
-            int j = i + 1; 
+            int j = i - 1; 
             
-            while (j > 0 && unsorted[j] > item) { 
+            while (j >= 0 && unsorted[j] > item) { 
                 unsorted[j + 1] = unsorted[j];
                 j = j - 1;
             }
             unsorted[j + 1] = item;
         }
     }
-    
+/**
+ * Here we used sort function 9 and made for loops to order the array
+ * @param unsorted 
+ */
     void sortFunction9(int unsorted[])
     {
-        int index = 0;
         int n = unsorted.length;
-        while (index <= n) { 
-            if (index == 0)
-                index++; 
-            if (unsorted[index-1] > unsorted[index])
-                index++;
-            else {
-                int temp = unsorted[index];
-                unsorted[index] = unsorted[index - 1];
-                unsorted[index - 1] = temp;
-                index--;
+        for (int index = 1; index < n; index++) {
+            int item = unsorted[index];
+            int j = index - 1;
+            while (j >= 0 && unsorted[j] > item) {
+                unsorted[j + 1] = unsorted[j];
+                j = j - 1;
             }
+            unsorted[j + 1] = item;
         }
     }
-    
+/**
+ * Here we are printing out the array to be displayed in order
+ * @param arr 
+ */
     void printArray(int arr[])
     {
         int n = arr.length;
@@ -57,8 +69,11 @@ public class LabTest1 {
             System.out.print(arr[i]+" ");
         System.out.println();
     }
- 
-    
+
+    /**
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         LabTest1 obj = new LabTest1();
         
@@ -76,7 +91,5 @@ public class LabTest1 {
         obj.sortFunction9(unsorted9);
         System.out.println("Sorted array9 : ");
         obj.printArray(unsorted9);
-       
     }
-    
 }
