f458f49e8094ebf8d2993c4b152ef14cb4d5d299
carterrows <carter.rows@gmail.com>
2023-02-09 12:54:03 -0500

    completed

--- src/labtest1/LabTest1.java
+++ src/labtest1/LabTest1.java
@@ -1,14 +1,21 @@
 package labtest1;
 
+/**
+ *
+ * @author Carter Rows - 1170615
+ * @since February 9th, 2023
+ * 
+ */
+
 public class LabTest1 {
 	
     void sortFunction7(int unsorted[]) 
     {
         int len = unsorted.length;
-        for (int i = 0; i <= len; i++) 
+        for (int i = 0; i < len-1; i++) 
         { 
             int index = i;
-            for (int j = i+1; j < len-1; j++) 
+            for (int j = i+1; j < len; j++) 
                 if (unsorted[j] < unsorted[index])
                     index = j;
             int t = unsorted[index];
@@ -20,11 +27,11 @@ public class LabTest1 {
     void sortFunction8(int unsorted[]) 
     {
         int ln = unsorted.length;
-        for (int i = 1; i < ln; ++i) {
+        for (int i = 0; i < ln; ++i) {
             int item = unsorted[i];
-            int j = i + 1; 
+            int j = i - 1; 
             
-            while (j > 0 && unsorted[j] > item) { 
+            while (j >= 0 && unsorted[j] > item) { 
                 unsorted[j + 1] = unsorted[j];
                 j = j - 1;
             }
@@ -34,19 +41,17 @@ public class LabTest1 {
     
     void sortFunction9(int unsorted[])
     {
-        int index = 0;
+        int index = 1;
         int n = unsorted.length;
-        while (index <= n) { 
-            if (index == 0)
-                index++; 
-            if (unsorted[index-1] > unsorted[index])
-                index++;
-            else {
-                int temp = unsorted[index];
-                unsorted[index] = unsorted[index - 1];
-                unsorted[index - 1] = temp;
-                index--;
+        while (index < n) { 
+            int key = unsorted[index];
+            int i = index - 1;
+            while (i >= 0 && unsorted[i] > key) {
+                unsorted[i+1] = unsorted[i];
+                i = i - 1;
             }
+            unsorted[i+1] = key;
+            index = index + 1;
         }
     }
     
@@ -57,7 +62,6 @@ public class LabTest1 {
             System.out.print(arr[i]+" ");
         System.out.println();
     }
- 
     
     public static void main(String[] args) {
         LabTest1 obj = new LabTest1();
@@ -77,6 +81,5 @@ public class LabTest1 {
         System.out.println("Sorted array9 : ");
         obj.printArray(unsorted9);
        
-    }
-    
+    }  
 }
