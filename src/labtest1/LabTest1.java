7a13849f945964f680bbeac93d247c67dfadfe12
Waleed Hawwari <waleedhawwari.12@gmail.com>
2023-02-09 13:07:39 -0500

    Waleed Hawwari 1153191 LabTest1

--- src/labtest1/LabTest1.java
+++ src/labtest1/LabTest1.java
@@ -1,14 +1,26 @@
 package labtest1;
 
+/**
+@author WaleedHawwari
+@since 2023-02-09
+@version 1.0
+
+*/
+
 public class LabTest1 {
 	
+    /**
+    * This method sorts an array of integers in ascending order.
+    * 
+    * @param unsorted The unsorted array of integers.
+    */
     void sortFunction7(int unsorted[]) 
     {
         int len = unsorted.length;
-        for (int i = 0; i <= len; i++) 
+        for (int i = 0; i < len; i++) 
         { 
             int index = i;
-            for (int j = i+1; j < len-1; j++) 
+            for (int j = i+1; j < len; j++) 
                 if (unsorted[j] < unsorted[index])
                     index = j;
             int t = unsorted[index];
@@ -16,39 +28,43 @@ public class LabTest1 {
             unsorted[i] = t;
         }
     }
- 
-    void sortFunction8(int unsorted[]) 
-    {
-        int ln = unsorted.length;
-        for (int i = 1; i < ln; ++i) {
-            int item = unsorted[i];
-            int j = i + 1; 
-            
-            while (j > 0 && unsorted[j] > item) { 
-                unsorted[j + 1] = unsorted[j];
-                j = j - 1;
-            }
-            unsorted[j + 1] = item;
+  /**
+    * This method sorts an array of integers in ascending order.
+    * 
+    * @param unsorted The unsorted array of integers.
+    */
+   void sortFunction8(int unsorted[]) 
+{
+    int len = unsorted.length;
+    for (int i = 1; i < len; ++i) {
+        int item = unsorted[i];
+        int j = i - 1; 
+        
+        while (j >= 0 && unsorted[j] > item) { 
+            unsorted[j + 1] = unsorted[j];
+            j = j - 1;
+        }
+        unsorted[j + 1] = item;
+    }
+}
+    /**
+    * This method sorts an array of integers in ascending order.
+    * 
+    * @param unsorted The unsorted array of integers.
+    */ 
+  void sortFunction9(int unsorted[])
+{
+    int n = unsorted.length;
+    for (int i = 1; i < n; i++) {
+        int j = i;
+        while (j > 0 && unsorted[j - 1] > unsorted[j]) {
+            int temp = unsorted[j];
+            unsorted[j] = unsorted[j - 1];
+            unsorted[j - 1] = temp;
+            j--;
         }
     }
-    
-    void sortFunction9(int unsorted[])
-    {
-        int index = 0;
-        int n = unsorted.length;
-        while (index <= n) { 
-            if (index == 0)
-                index++; 
-            if (unsorted[index-1] > unsorted[index])
-                index++;
-            else {
-                int temp = unsorted[index];
-                unsorted[index] = unsorted[index - 1];
-                unsorted[index - 1] = temp;
-                index--;
-            }
-        }
-    }
+}
     
     void printArray(int arr[])
     {
